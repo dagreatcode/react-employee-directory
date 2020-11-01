f9b3b89a9e5d5b807caf70ba690de3068689dd08
dagreatcode <dagreatcode@gmail.com>
2020-11-01 01:25:03 -0500

    updated axios call to fix error of just one

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -3,18 +3,18 @@ function Employees(props) {
         <table class="table">
             <thead>
                 <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
+
+                <th scope="col">Name</th>
+                <th scope="col">Phone</th>
                 <th scope="col">Email</th>
                 <th scope="col">DOB</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                <th scope="row">#</th>
-                <td>{props.firstname}</td>
-                <td>{props.lastname}</td>
+
+                <td>{props.name}</td>
+                <td>{props.phone}</td>
                 <td>{props.email}</td>
                 <td>{props.dob}</td>
                 </tr>
