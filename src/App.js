64215757339740815e7d2e24322dbd7e194db6b8
dagreatcode <dagreatcode@gmail.com>
2020-11-01 01:26:41 -0400

    built header

--- src/App.js
+++ src/App.js
@@ -1,1 +1,42 @@
-import searchEmployee from "./components/searchEmployee";
+// import searchEmployee from "./components/searchEmployee";
+
+function App(props) {
+  return (
+    <>
+      <header
+        style={{
+          backgroundColor: "rgb(0,24,65)",
+          height: 130,
+          borderBottom: "5px solid red",
+        }}
+        className="header"
+      >
+        <h1
+          style={{
+            color: "white",
+            textAlign: "center",
+            fontFamily: "Reem Kufi",
+          }}
+        >
+          Employee Directory
+        </h1>
+        <p
+          style={{
+            color: "white",
+            textAlign: "center",
+            fontFamily: "Reem Kufi",
+          }}
+        >
+          Use the search box to narrow
+          your results.
+        </p>
+      </header>
+
+      <div>
+                 {/* <searchEmployee /> */}
+      </div>
+    </>
+  );
+}
+
+export default App;
