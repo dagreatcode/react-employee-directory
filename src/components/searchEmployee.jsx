eebf9ba226b246636fef21d1924ab8b9b5c62332
dagreatcode <dagreatcode@gmail.com>
2020-11-01 01:41:37 -0400

    working on searchEmployee

---
+++ src/components/searchEmployee.jsx
@@ -0,0 +1,28 @@
+const Employees = (props) => {
+    return (
+        <div>
+            <table class="table">
+                <thead>
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">DOB</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <th scope="row">#</th>
+                    <td>{props.firstname}</td>
+                    <td>{props.lastname}</td>
+                    <td>{props.email}</td>
+                    <td>{props.dob}</td>
+                    </tr>
+                </tbody>
+                </table>
+        </div>
+    );
+};
+
+export default Employees;
